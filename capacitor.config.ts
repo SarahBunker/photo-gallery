4d19a7c582d593d9a4bb87f66f0bfa295a528723
Sarah Bunker <ChemEnCoder@gmail.com>
2024-02-19 19:27:27 -0700

    updating build and adding some SDK stuff to link to app flow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,20 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "photo-gallery",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: "042a1261",
+      channel: "Production",
+      autoUpdateMethod: "background",
+      maxVersions: 2,
+    },
+  },
 };
 
 export default config;
